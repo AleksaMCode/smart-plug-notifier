15eed0615c1b14c00f3523e4acac3a459a384132
AleksaMCode <aleksamcode@gmail.com>
2025-08-18 19:32:47 +0200

    Add rebbitmq information

--- tapo_service/settings.template.py
+++ tapo_service/settings.template.py
@@ -9,10 +9,17 @@ load_dotenv()
 TAPO_EMAIL = os.getenv("TAPO_EMAIL")
 TAPO_PASSWORD = os.getenv("TAPO_PASSWORD")
 
+# Environment variables for Rabbit MQ
+RABBITMQ_USERNAME = os.getenv("RABBITMQ_USERNAME")
+RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD")
+
 if not TAPO_EMAIL or not TAPO_PASSWORD:
     raise RuntimeError(
         "TAPO_EMAIL and TAPO_PASSWORD must be set in .env or environment variables."
     )
+
+if not RABBITMQ_USERNAME or not RABBITMQ_USERNAME:
+    raise RuntimeError("RABBITMQ_USERNAME and RABBITMQ_PASSWORD must be set in .env or environment variables.")
 
 # Network mask used to discover devices - used as a Target protocol address (TPA) for ARP messages
 # E.g., 192.168.1.1/24
@@ -30,4 +37,11 @@ SERVER = {
     "localhost": "0.0.0.0",
     "host": "127.0.0.1",
     "port": 3_001,
+}
+
+# Rabbit MQ configuration.
+RABBIT_MQ = {
+    "host": "localhost",
+    "port": 5672,
+    "queue": "smart_device",
 }

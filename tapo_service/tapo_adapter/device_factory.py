e388fbf5c46bc7f1d6f58bf36133543166b74651
AleksaMCode <aleksamcode@gmail.com>
2025-08-18 16:39:19 +0200

    Add Device Factory

---
+++ tapo_service/tapo_adapter/device_factory.py
@@ -0,0 +1,22 @@
+from abc import ABCMeta
+
+from tapo import ApiClient
+from tapo_adapter.device import Device
+from tapo_adapter.plug_p110 import PlugP110
+from tapo_adapter.tapo_device_type import TapoDeviceType
+
+
+class DeviceFactory(metaclass=ABCMeta):
+    """Abstract factory for Tapo devices."""
+
+    @staticmethod
+    async def create(
+        name: str, type: TapoDeviceType, mac: str, client: ApiClient
+    ) -> Device:
+        match type:
+            case TapoDeviceType.P110:
+                device = PlugP110(name, mac, client)
+                await device.init()
+                return device
+            case _:
+                raise RuntimeError(f"Unsupported device type: {type}")

aa2da827d479371c4295d2820e8db67f05c34e85
AleksaMCode <aleksamcode@gmail.com>
2025-08-19 07:54:58 +0200

    Add RabbitMQ subscriber

---
+++ notification_service/rabbitmq_subscriber.py
@@ -0,0 +1,13 @@
+import json
+
+from rabbitmq_adapter.rabbitmq_adapter import RabbitMqAdapter
+
+
+class RabbitMqSubscriber(RabbitMqAdapter):
+    async def amqp_handler(self, callback):
+        await self.connect()
+        async with self._channel_queue.iterator() as queue_iter:
+            async for message in queue_iter:
+                async with message.process():
+                    payload = json.loads(message.body.decode())
+                    callback(payload)

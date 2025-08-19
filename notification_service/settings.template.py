2104bb0d84db9b18d5b09d5d19e7d41d8d2a0c85
AleksaMCode <aleksamcode@gmail.com>
2025-08-19 08:04:16 +0200

    Update port

--- notification_service/settings.template.py
+++ notification_service/settings.template.py
@@ -19,7 +19,7 @@ SERVER = {
     # localhost address is needed to make `uvicorn` server accessible on the local network using different devices.
     "localhost": "0.0.0.0",
     "host": "127.0.0.1",
-    "port": 3_001,
+    "port": 3_002,
 }
 
 # Rabbit MQ configuration.
